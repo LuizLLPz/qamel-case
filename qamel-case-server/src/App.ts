d7a87ed19d0df73b2a0af949ee4689dee70f08d1
LuizLLPz <luizllp321@gmail.com>
2022-02-06 06:54:39 -0300

    Downgrade @types/graphql para evitar problemas de compatibildade + implementação final api simples

--- qamel-case-server/src/App.ts
+++ qamel-case-server/src/App.ts
@@ -1,7 +1,8 @@
+import "reflect-metadata";
 import App from "express";
-import buildSchema from "type-graphql";
-import { User } from "graphql/resolvers/User";
-import { ApolloServer, gql } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { User } from "./graphql/resolvers/User";
+import { ApolloServer } from "apollo-server-express";
 
 import { config } from "dotenv-safe";
 import { PrismaClient } from "@prisma/client";
@@ -11,24 +12,20 @@ const prisma = new PrismaClient()
 async function init () {
 
 	config();
-	const users = await prisma.user.findMany();
-	console.log(users);
 	const schema = await buildSchema({
     	resolvers: [
     		User
     	],
+    	emitSchemaFile: true,
+    	validate: false,
   	});
-
-	const apollo = new ApolloServer({
-		schema
-	});
-
+  	const apollo = new ApolloServer({schema})
+  	await apollo.start();
 	const app = App();
-	await apollo.start();	
-	apollo.applyMiddleware({app});
 	app.listen(process.env.SERVER_PORT, function (){
 		console.log(`Server started on port ${process.env.SERVER_PORT}`);
 	});
+	apollo.applyMiddleware({app});
 
 }
 
